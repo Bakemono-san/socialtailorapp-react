db51ff2b111e0c4ef37e13dbfa19e9820b48c61c
bakemono-san <mousdef34@gmail.com>
2024-10-03 13:36:19 +0000

    feat(frontend) : changed the disposition of the post form and added a preview there

--- src/Components/Forms/Post.jsx
+++ src/Components/Forms/Post.jsx
@@ -11,53 +11,60 @@ const ModelForm = () => {
 
     return (
         <>
-            <form className="flex flex-col gap-4 w-full bg-white p-2 rounded-lg shadow-lg">
-                <div className="flex flex-col sm:flex-row gap-4 justify-between">
-                    <div className="flex-1">
-                        <label htmlFor="titre" className="font-semibold">Titre</label>
-                        <input
-                            type="text"
-                            id="titre"
-                            name="titre"
-                            className="w-full border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
-                            required
-                        />
+            <form className='flex gap-4 h-40'>
+                <div className="flex flex-col gap-4 w-full md:w-4/5 bg-white p-2 rounded-lg shadow-lg">
+
+                    <div className="flex flex-col sm:flex-row gap-4 justify-between">
+                        <div className="flex-1">
+                            <label htmlFor="titre" className="font-semibold">Titre</label>
+                            <input
+                                type="text"
+                                id="titre"
+                                name="titre"
+                                className="w-full border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
+                                required
+                            />
+                        </div>
+                        <div className="flex-1">
+                            <label htmlFor="description" className="font-semibold">Description</label>
+                            <input
+                                type="text"
+                                id="description"
+                                name="description"
+                                className="w-full border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
+                                required
+                            />
+                        </div>
                     </div>
-                    <div className="flex-1">
-                        <label htmlFor="description" className="font-semibold">Description</label>
-                        <input
-                            type="text"
-                            id="description"
-                            name="description"
-                            className="w-full border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
-                            required
-                        />
+                    <div className='flex justify-between'>
+                        <div>
+
+                            <label htmlFor="modelId" className="font-semibold">Model</label>
+                            <button
+                                type="button"
+                                className="ml-4 bg-blue-500 text-white px-3 py-1 rounded-lg hover:bg-blue-600"
+                                onClick={() => setIsModalOpen(true)}
+                            >
+                                Choose Model
+                            </button>
+                            <input
+                                type="text"
+                                id="modelId"
+                                name="modelId"
+                                value={modelId}
+                                onChange={(e) => setModelId(e.target.value)}
+                                className="hidden"
+                                required
+                            />
+                            {modelId && <p className="mt-2 text-gray-600">Selected Model: {modelId}</p>}
+                        </div>
+                        <button type='submit' className='btn btn-success text-white'>Post</button>
+
                     </div>
                 </div>
-                <div className='flex justify-between'>
-                    <div>
 
-                        <label htmlFor="modelId" className="font-semibold">Model</label>
-                        <button
-                            type="button"
-                            className="ml-4 bg-blue-500 text-white px-3 py-1 rounded-lg hover:bg-blue-600"
-                            onClick={() => setIsModalOpen(true)}
-                        >
-                            Choose Model
-                        </button>
-                        <input
-                            type="text"
-                            id="modelId"
-                            name="modelId"
-                            value={modelId}
-                            onChange={(e) => setModelId(e.target.value)}
-                            className="hidden"
-                            required
-                        />
-                        {modelId && <p className="mt-2 text-gray-600">Selected Model: {modelId}</p>}
-                    </div>
-                    <button type='submit' className='btn btn-success text-white'>Post</button>
-
+                <div className='md:block hidden'>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQVv0X1OiwK4BXsh6RT2w1jXWPS3LoHJ74_Q&s" alt="" className='h-full w-full' />
                 </div>
 
 
