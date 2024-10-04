76dba8531d5dcf871f426585dc6c75ed673897b6
ZahraThiaw <fatimatathiaw6@gmail.com>
2024-10-04 11:13:37 +0000

    Ajouter formulaire ajouter ou modifier mesure et Afficher, paginer et filtrer la liste des tailleurs sous forme de card et peut etre sous forme de tableau

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Sidebar from "./Components/Sidebar";
 import Discussion from "./Pages/Discussion";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainPage from "./Pages/MainPage";
+import TailleursListe from "./Pages/TailleursListe";
+import Parameters from "./Pages/Parameters";
+import Measurements from "./Pages/Measurements";
 
 
 function App() {
@@ -15,6 +18,9 @@ function App() {
           <Routes>
             <Route path="/" element={<MainPage />} />
             <Route path="/discussion" element={<Discussion />} />
+            <Route path="/tailleurs" element={<TailleursListe />} />
+            <Route path="/params" element={<Parameters />} />
+            <Route path="/measurements" element={<Measurements/>} />
           </Routes>
         </div>
       </div>
