60aa50c6ec920de41716c7900b9dc7519f6ab102
tellykadiatou685 <kadiatoutelly685@gmail.com>
2024-10-04 10:55:21 +0000

    creer formulaire story et creer model

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import Header from "./Components/Header";
 import Sidebar from "./Components/Sidebar";
+import DiscussionPage from "./Pages/DiscussionPage";
+import Models from "./Pages/Models";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainPage from "./Pages/MainPage";
-import ListDiscussion from "./Pages/ListDiscussion";
-import DiscussionPage from "./Pages/DiscussionPage";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+
 
 
 function App() {
   return (
     <Router>
-      <div className="bg-blue-100 pb-16 h-dvh overflow-hidden">
+      <div className="bg-blue-100 pb-16 h-full">
         <Header />
         <div className="md:p-4 md:flex">
           <Sidebar color="bg-blue-700" />
           <Routes>
             <Route path="/" element={<MainPage />} />
-            <Route path="/discussion" element={<ListDiscussion />} />
-            <Route path={`/discussion/:id`} element={<DiscussionPage />} />
+            <Route path="/discussion" element={<DiscussionPage />} />
+            <Route path="/Models" element={<Models/>} />
 
           </Routes>
         </div>
