96c9f2122b0df704ff45cf01f2e717363517e12d
bakemono-san <mousdef34@gmail.com>
2024-10-07 13:17:14 +0000

    feat(frontend) : added the login page

--- src/App.js
+++ src/App.js
@@ -13,36 +13,42 @@ import Panier from "./Pages/Panier";
 import Ranking from "./Pages/Ranking";
 import ListDiscussion from "./Pages/ListDiscussion";
 import RightSideBar from "./Components/RightSideBar";
+import Login from "./Pages/Login";
 
 function App() {
   return (
     <Router>
-      <div className="bg-blue-100 h-screen flex flex-col overflow-hidden">
-        <Header />
-        <div className="flex flex-1 md:flex-row flex-col-reverse overflow-hidden md:p-4 md:gap-8 lg:gap-24">
-          {/* Sidebar */}
-          <Sidebar color="bg-blue-700" />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={
+          <div className="bg-blue-100 h-screen flex flex-col overflow-hidden">
+            <Header />
+            <div className="flex flex-1 md:flex-row flex-col-reverse overflow-hidden md:p-4 md:gap-8 lg:gap-24">
+              {/* Sidebar */}
+              <Sidebar color="bg-blue-700" />
 
-          <div className="flex flex-col flex-1 overflow-hidden h-full">
-            <Routes>
-              <Route path="/" element={<MainPage />} />
-              
-              <Route path="/tailleurs" element={<TailleursListe />} />
-              <Route path="/params" element={<Parameters />} />
-              <Route path="/measurements" element={<Measurements />} />
-              <Route path="/achatcredit" element={<AchatCredit />} />
-              <Route path="/listesouhait" element={<ListeSouhait />} />
-              <Route path="/Models" element={<Models />} />
-              <Route path="/panier" element={<Panier />} />
-              <Route path="/rang" element={<Ranking />} />
-              <Route path="/discussion" element={<ListDiscussion />} />
-              <Route path="/discussion/:id" element={<DiscussionPage />} />
-            </Routes>
-          </div>
+              <div className="flex flex-col flex-1 overflow-hidden h-full">
+                <Routes>
+                  <Route path="/" element={<MainPage />} />
 
-          <RightSideBar color="bg-white" />
-        </div>
-      </div>
+                  <Route path="/tailleurs" element={<TailleursListe />} />
+                  <Route path="/params" element={<Parameters />} />
+                  <Route path="/measurements" element={<Measurements />} />
+                  <Route path="/achatcredit" element={<AchatCredit />} />
+                  <Route path="/listesouhait" element={<ListeSouhait />} />
+                  <Route path="/Models" element={<Models />} />
+                  <Route path="/panier" element={<Panier />} />
+                  <Route path="/rang" element={<Ranking />} />
+                  <Route path="/discussion" element={<ListDiscussion />} />
+                  <Route path="/discussion/:id" element={<DiscussionPage />} />
+                </Routes>
+              </div>
+
+              <RightSideBar color="bg-white" />
+            </div>
+          </div> 
+        } />
+        </Routes>
     </Router>
   );
 }
