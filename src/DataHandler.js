18d958fc08a3e4117e81b66353d66db93b50eb72
bakemono-san <mousdef34@gmail.com>
2024-10-08 18:13:38 +0000

    feta(authentication): update the handler to add the token in the header while sending request

--- src/DataHandler.js
+++ src/DataHandler.js
@@ -1,43 +1,70 @@
 import axios from "axios";
 
-export default class DataHandler{
-    static postData(url,data){
-       return axios.post(url,data)
-        .then(response => {
-            console.log(response.data);
+export default class DataHandler {
+    static api = axios.create({
+        baseURL: "http://localhost:3004", // Your backend API base URL
+    });
+
+    static setupInterceptors() {
+        this.api.interceptors.request.use(
+            (config) => {
+                const token = localStorage.getItem("token");
+                
+                
+                if (token) {
+                    config.headers["Authorization"] = `Bearer ${token}`;
+                    console.log(config.headers);
+                }
+
+                return config;
+            },
+            (error) => {
+                // Handle the request error here
+                return Promise.reject(error);
+            }
+        );
+    }
+
+    static async postData(url, data) {
+        try {
+            const response = await this.api.post(url, data);
             return response.data;
-        })
-        .catch(error => {
-            console.log(error);
-            throw error;
-        })
-    }
-
-    static async getDatas(url){
-        try{
-            const datas = await axios.get(url);
-            return datas;
-        }catch(error){
+        } catch (error) {
+            console.error(error);
             throw error;
         }
     }
 
-    static async updateData(url,data){
-        try{
-            const datas = await axios.put(url,data);
-            return datas;
-        }catch(error){
+    static async getDatas(url) {
+        try {
+            const response = await this.api.get(url);
+            return response.data;
+        } catch (error) {
+            console.error(error);
             throw error;
         }
     }
 
-    static async deleteData(url){
-        try{
-            const datas = await axios.delete(url);
-            return datas;
-        }catch(error){
+    static async updateData(url, data) {
+        try {
+            const response = await this.api.put(url, data);
+            return response.data;
+        } catch (error) {
+            console.error(error);
             throw error;
         }
     }
-    
-}   
+
+    static async deleteData(url) {
+        try {
+            const response = await this.api.delete(url);
+            return response.data;
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    }
+}
+
+// Call setupInterceptors once during app initialization
+DataHandler.setupInterceptors();
