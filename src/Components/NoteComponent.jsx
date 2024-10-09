acdd7aad7371a78633eafa46430f1616ccab8f58
Dimkha <dimkha516@gmail.com>
2024-10-09 15:47:49 +0000

    design page notation

--- src/Components/NoteComponent.jsx
+++ src/Components/NoteComponent.jsx
@@ -1,11 +1,50 @@
-import React from 'react';
+import React from "react";
 
-const NoteComponent = ({note}) => {
-    return (
+const NoteComponent = ({ tailleur }) => {
+  return (
+    <div className="bg-white shadow-lg rounded-lg overflow-hidden flex my-4 max-w-4xl mx-auto">
+      {/* Image du produit */}
+      <img
+        className="w-32 h-32 object-cover"
+        src={tailleur.image}
+        alt={tailleur.name}
+      />
+
+      {/* Détails du produit */}
+      <div className="p-4 flex flex-col justify-between">
         <div>
-            <h2>Notation</h2>
+          {/* Nom du produit */}
+          <h2 className="text-xl font-bold text-gray-800">{tailleur.name}</h2>
+
+          {/* Description du produit */}
+          <p className="text-gray-600 mt-2">{tailleur.description}</p>
         </div>
-    );
+
+        {/* Évaluation par étoiles */}
+        <div className="mt-4 flex">
+          {[...Array(5)].map((_, index) => (
+            <svg
+              key={index}
+              xmlns="http://www.w3.org/2000/svg"
+              fill={index < tailleur.rank ? "currentColor" : "none"}
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className={`h-5 w-5 ${
+                index < tailleur.rank ? "text-yellow-500" : "text-gray-300"
+              }`}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 17.25l-6.172 3.246 1.178-6.872-4.993-4.87 6.897-1.002L12 2.75l3.09 6.002 6.897 1.002-4.993 4.87 1.178 6.872L12 17.25z"
+              />
+            </svg>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NoteComponent;
--- src/Pages/Notation.jsx
+++ src/Pages/Notation.jsx
@@ -1,9 +1,35 @@
 import React from "react";
+import NoteComponent from "../Components/NoteComponent";
 
 const Notation = () => {
+  const tailleurs = [
+    {
+      image:
+        "https://res.cloudinary.com/dytchfsin/image/upload/v1728488367/persons/pybmk26zm7kjk2kjgsml.jpg",
+      description: "Mame Fatou Fashion",
+      rank: 4,
+    },
+
+    {
+      image:
+        "https://res.cloudinary.com/dytchfsin/image/upload/v1728488365/persons/xs45tfdhcpkd2jxgoyor.jpg",
+      description: "Dikha Shop",
+      rank: 3,
+    },
+
+    {
+      image:
+        "https://res.cloudinary.com/dytchfsin/image/upload/v1728488361/persons/ocavdlfhkukmdb8w9jra.jpg",
+      description: "Baye Mbaye Couture",
+      rank: 2,
+    },
+  ];
+
   return (
     <div>
-      <h2>Notation</h2>
+      {tailleurs.map((tailleur, index) => (
+        <NoteComponent key={index} tailleur={tailleur} />
+      ))}
     </div>
   );
 };
