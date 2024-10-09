bbea828b352bef78d6e3360bcc4f213ef88cc4fb
bakemono-san <mousdef34@gmail.com>
2024-10-09 16:13:37 +0000

    feat(frontend) " ajouter la fonctionnalite pour creer un nouveau post"
    closes #41

--- src/Components/Forms/Post.jsx
+++ src/Components/Forms/Post.jsx
@@ -1,29 +1,48 @@
 import React, { useContext, useState } from 'react';
 import { DataContext } from '../../App';
+import DataHandler from '../../DataHandler';
 
 const ModelForm = () => {
     const { value, setValue } = useContext(DataContext);
     const [models, setModels] = useState(value.models);
     const [modelId, setModelId] = useState();
+    const [selectedModel,setSelectedModel] = useState();
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const [title,setTitle] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(title,modelId);
+        DataHandler.postData("http://localhost:3004/post/create",{modelId:modelId,titre:title,description: "hello world"})
+        .then((data) => {
+            if (data) {
+                alert(data.message)
+                alert(data.error)
+                setTitle("");
+                setModelId("");
+            }
+        })
+        .catch((error) => {
+            console.error(error.error);
+        });
+    }
 
 
-
-
-    const handleSelectModel = (model) => {
-        setModelId(model);
+    const handleSelectModel = (model,id) => {
+        setSelectedModel(model);
+        setModelId(id);
         setIsModalOpen(false); // Close the modal after selecting a model
     };
 
     return (
         <>
-            <form className='flex flex-col gap-2  bg-white p-4 rounded-lg shadow-lg'>
+            <form className='flex flex-col gap-2  bg-white p-4 rounded-lg shadow-lg' onSubmit={handleSubmit}>
                 <div className='flex justify-between items-center '>
                     <h1 className='text-lg font-semibold text-gray-600'>Create a Post</h1>
                     <div className='flex flex-col h-content items-center justify-center'>...</div>
                 </div>
                 <div>
-                    <input type="text" className='w-full bg-gray-200 p-2 rounded' placeholder='write something here...' />
+                    <input type="text" className='w-full bg-gray-200 p-2 rounded' value={title} onChange={(e) => setTitle(e.target.value)} placeholder='write something here...' />
                 </div>
                 <input
                     type="text"
@@ -34,7 +53,7 @@ const ModelForm = () => {
                     className="hidden"
                     required
                 />
-                {modelId && <p className="mt-2 text-gray-600">Selected Model: {modelId}</p>}
+                {modelId && <p className="mt-2 text-gray-600">Selected Model: {selectedModel}</p>}
                 <div className='p-1 flex justify-between items-center'>
 
                     <button
@@ -46,69 +65,13 @@ const ModelForm = () => {
                     </button>
 
                     <button
-                        type="button"
+                        type="submit"
                         className=" bg-green-500 text-white px-3 py-1  w-fit rounded-lg hover:bg-green-600"
                     >
                         publish
                     </button>
 
                 </div>
-
-
-                {/* <div className="flex flex-col gap-4 w-full md:w-4/5 bg-white p-2 rounded-lg shadow-lg">
-
-                    <div className="flex sm:flex-row gap-4 justify-between">
-                        <div className="flex-1">
-                            <label htmlFor="titre" className="font-semibold">Titre</label>
-                            <input
-                                type="text"
-                                id="titre"
-                                name="titre"
-                                className="w-full border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
-                                required
-                            />
-                        </div>
-                        <div className="flex-1">
-                            <label htmlFor="description" className="font-semibold">Description</label>
-                            <input
-                                type="text"
-                                id="description"
-                                name="description"
-                                className="w-full border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
-                                required
-                            />
-                        </div>
-                    </div>
-                    <div className='flex justify-between'>
-                        <div>
-
-                            <label htmlFor="modelId" className="font-semibold">Model</label>
-                            <button
-                                type="button"
-                                className="ml-4 bg-blue-500 text-white px-3 py-1 rounded-lg hover:bg-blue-600"
-                                onClick={() => setIsModalOpen(true)}
-                            >
-                                Choose Model
-                            </button>
-                            <input
-                                type="text"
-                                id="modelId"
-                                name="modelId"
-                                value={modelId}
-                                onChange={(e) => setModelId(e.target.value)}
-                                className="hidden"
-                                required
-                            />
-                            {modelId && <p className="mt-2 text-gray-600">Selected Model: {modelId}</p>}
-                        </div>
-                        <button type='submit' className='btn btn-success text-white'>Post</button>
-
-                    </div>
-                </div>
-
-                <div className='md:block hidden'>
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQVv0X1OiwK4BXsh6RT2w1jXWPS3LoHJ74_Q&s" alt="" className='h-full w-full' />
-                </div> */}
 
 
             </form>
@@ -122,9 +85,10 @@ const ModelForm = () => {
 
                             {models.map((model) => {
                                 return <button
+                                    key={model.id}
                                     type="button"
                                     className="w-full bg-gray-100 hover:bg-gray-200 p-2 mb-2 rounded-lg"
-                                    onClick={() => handleSelectModel(model.libelle)}
+                                    onClick={() => handleSelectModel(model.libelle,model.id)}
                                 >
                                     {model.libelle}
                                 </button>
--- src/Components/RightSideBar.jsx
+++ src/Components/RightSideBar.jsx
@@ -8,8 +8,8 @@ export default function Sidebar(props) {
   const { value, setDiscussion } = useContext(DataContext)
 
   return (
-    <div className={props.color + `  bg-[#3b5999] hidden md:flex md:py-4 md:px-5 md:flex-col md:gap-10 md:h-full text-white md:rounded w-full min-h-12 shadow md:w-24`}>
-      <div className='hidden md:block'>
+    <div className={props.color + `  bg-white hidden md:flex md:py-4 md:px-5 md:flex-col md:gap-10 md:h-full text-white md:rounded w-full min-h-12 shadow md:w-24`}>
+      <div className='hidden md:block bg-[#3b5999] rounded'>
         <SidebarItem icon={faSearch} path="/" />
       </div>
       <hr />
