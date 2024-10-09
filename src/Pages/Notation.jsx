9369c89d679a864627395cc0b7ecd881bc995a40
Dimkha <dimkha516@gmail.com>
2024-10-09 16:54:15 +0000

    update

--- src/Pages/Notation.jsx
+++ src/Pages/Notation.jsx
@@ -5,13 +5,6 @@ const Notation = () => {
   const tailleurs = [
     {
       image:
-        "https://res.cloudinary.com/dytchfsin/image/upload/v1728488367/persons/pybmk26zm7kjk2kjgsml.jpg",
-      description: "Mame Fatou Fashion",
-      rank: 4,
-    },
-
-    {
-      image:
         "https://res.cloudinary.com/dytchfsin/image/upload/v1728488365/persons/xs45tfdhcpkd2jxgoyor.jpg",
       description: "Dikha Shop",
       rank: 3,
@@ -23,13 +16,24 @@ const Notation = () => {
       description: "Baye Mbaye Couture",
       rank: 2,
     },
-  ];
+    {
+      image:
+        "https://res.cloudinary.com/dytchfsin/image/upload/v1728488367/persons/pybmk26zm7kjk2kjgsml.jpg",
+      description: "Mame Fatou Fashion",
+      rank: 5,
+    },
+    
+    
+
+];
 
   return (
     <div>
-      {tailleurs.map((tailleur, index) => (
-        <NoteComponent key={index} tailleur={tailleur} />
-      ))}
+      {tailleurs
+        .sort((a, b) => b.rank - a.rank)
+        .map((tailleur, index) => (
+          <NoteComponent key={index} tailleur={tailleur} />
+        ))}
     </div>
   );
 };
